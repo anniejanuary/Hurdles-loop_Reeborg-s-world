769f782c91a5e14c5ae2e470d7d94cec08730a4f
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-08-16 11:55:12 +0200

    Create Hurdles loop_Reeborg's world.py

---
+++ Hurdles loop_Reeborg's world.py
@@ -0,0 +1,18 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+for x in range(6):
+    move()
+    jump()
+   
