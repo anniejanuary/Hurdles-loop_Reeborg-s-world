525dde69d47b5c257f8ac459ed29c6428fd726b2
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-08-17 15:10:00 +0200

    Update Hurdles loop_Reeborg's world.py

--- Hurdles loop_Reeborg's world.py
+++ Hurdles loop_Reeborg's world.py
@@ -11,8 +11,23 @@ def jump():
     turn_right()
     move()
     turn_left()
+
     
+#        with FOR LOOP    
+
 for x in range(6):
     move()
     jump()
+    
+#        with WHILE LOOP
    
+#number_of_hurdles = 6
+#while number_of_hurdles > 0:
+#    jump()
+#    number_of_hurdles -= 1
+
+
+#        with at_goal() WHILE LOOP (function applies to Reeborgs world only)
+
+#while at_goal() == False:
+#    jump()
